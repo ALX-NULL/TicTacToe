536cf9dafcedb486462e7248cca0d2301866b14d
mahmoud malek <information.mahmoud@gmail.com>
2024-08-30 17:15:12 +0300

    done :hammer:

--- app/static/scripts/login.js
+++ app/static/scripts/login.js
@@ -17,36 +17,36 @@ const passwordError = document.getElementById('password_error');
 
 // add an event listener to the form
 form.addEventListener('submit', (e) => {
-	// initialize an array to store the error messages
-	const messages = [];
+  // initialize an array to store the error messages
+  const messages = [];
 
-	// check if the username input is empty
-	if (username.value === '' || username.value == null) {
-		messages.push('username is required');
-	}
+  // check if the username input is empty
+  if (username.value === '' || username.value == null) {
+    messages.push('username is required');
+  }
 
-	// check if the password input is empty
-	if (password.value === '' || password.value == null) {
-		messages.push('Password is required');
-	}
+  // check if the password input is empty
+  if (password.value === '' || password.value == null) {
+    messages.push('Password is required');
+  }
 
-	// check if the password is less than 6 characters
-	if (password.value.length < 6) {
-		messages.push('Password must be at least 6 characters');
-	}
+  // check if the password is less than 6 characters
+  if (password.value.length < 6) {
+    messages.push('Password must be at least 6 characters');
+  }
 
-	// check if the password is more than 20 characters
-	if (password.value.length > 20) {
-		messages.push('Password must be less than 20 characters');
-	}
+  // check if the password is more than 20 characters
+  if (password.value.length > 20) {
+    messages.push('Password must be less than 20 characters');
+  }
 
-	// check if there are any errors
-	if (messages.length > 0) {
-		// prevent the form from submitting
-		e.preventDefault();
+  // check if there are any errors
+  if (messages.length > 0) {
+    // prevent the form from submitting
+    e.preventDefault();
 
-		// display the error messages
-		usernameError.innerText = messages[0];
-		passwordError.innerText = messages[1];
-	}
+    // display the error messages
+    usernameError.innerText = messages[0];
+    passwordError.innerText = messages[1];
+  }
 });
